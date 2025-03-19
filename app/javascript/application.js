83674249f575f866ca85820d9a7c60038014d622
Gabriel Castro <gabocastrosemper28@gmail.com>
2025-03-19 04:46:39 -0500

    First commit

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit(); // Inicializa automáticamente todos los componentes de Materialize
+  });
